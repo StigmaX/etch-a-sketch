d0d7f2b08b4f9e25f14652361acbd1bec9d561f4
Wildan Kusnaedi <Wildank40@gmail.com>
2023-03-16 15:46:29 +0700

    add sketch resizer with input slider

--- script/script.js
+++ script/script.js
@@ -1,16 +1,24 @@
 const grid = document.querySelector('#sketch');
-const slider = document.querySelector('slider');
+const slider = document.querySelector('#slider');
 const color = document.querySelector('#pickedColor');
+const currentSketchSize = document.querySelector('.text');
+currentSketchSize.style.textAlign = 'center';
+currentSketchSize.style.fontWeight = 'bold';
+currentSketchSize.style.fontSize = '20px';
 
 let currentColor;
 let mousePressed = false;
-document.body.onmousedown = () => mousePressed =true;
+document.body.onmousedown = () => mousePressed = true;
 document.body.onmouseup = () => mousePressed = false;
+
+
+
 function setupSketch(size) {
     grid.style.gridTemplateColumns = `repeat(${size}, 1fr)`;
     grid.style.gridTemplateRows = `repeat(${size}, 1fr)`;
+    currentSketchSize.innerText = `${size} * ${size}`;
     for(i = 0; i<size*size; i++){
-        const row = document.createElement('div');
+        const row = document.createElement('div'); 
         row.classList.add('pixel');
         row.addEventListener('mouseover', changeColor);
         row.addEventListener('mousedown', changeColor);
@@ -19,7 +27,10 @@ function setupSketch(size) {
 }
 
 setupSketch(16);
-
+slider.addEventListener('input', (Event) => {
+    grid.innerText='';
+    setupSketch(Event.target.value);
+})
 
 color.addEventListener('change', (Event) => {
     currentColor = Event.target.value;
