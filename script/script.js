40ca2819ebd3daefbf8073cdb5c1ec6aabe210bd
Wildan Kusnaedi <Wildank40@gmail.com>
2023-03-16 14:51:01 +0700

    Add hover on each pixel of the sketch, add black color to sketch when mouse is pressed

--- script/script.js
+++ script/script.js
@@ -1,12 +1,33 @@
 const grid = document.querySelector('#sketch');
+const slider = document.querySelector('slider');
+const color = document.querySelector('#pickedColor');
 
+let currentColor;
+let mousePressed = false;
+document.body.onmousedown = () => mousePressed =true;
+document.body.onmouseup = () => mousePressed = false;
 function setupSketch(size) {
     grid.style.gridTemplateColumns = `repeat(${size}, 1fr)`;
     grid.style.gridTemplateRows = `repeat(${size}, 1fr)`;
     for(i = 0; i<size*size; i++){
         const row = document.createElement('div');
+        row.classList.add('pixel');
+        row.addEventListener('mouseover', changeColor);
+        row.addEventListener('mousedown', changeColor);
         grid.appendChild(row);
     }
 }
 
 setupSketch(16);
+
+
+color.addEventListener('change', (Event) => {
+    currentColor = Event.target.value;
+})
+
+function changeColor(e) {
+    if(e.type === 'mouseover' && !mousePressed) return;
+
+    else e.target.style.backgroundColor = '#000000';
+
+}
